9457709e1e4ee54ad3a1daa1cdc0640a6524df6b
Lauren Emms <lauren.emms@live.co.uk>
2018-04-03 15:57:57 +0800

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  task = Task.new(title: Faker::Hipster.word, details: Faker::Hipster.sentence)
+  task.save
+end
